c821862ca7352de851504b4b15b92b5a26bdaed7
raphaelkieling <unknown@example.com>
2019-02-27 14:30:14 -0300

    JavascriptModule updated now he accept a the last version of js.
    I need read the file and transform with babeljs,the result code has exports, then i need create a module instance to read it.

--- src/modules/javascript.js
+++ src/modules/javascript.js
@@ -1,9 +1,22 @@
+import fs from 'fs';
+import * as babel from '@babel/core';
+import Module from 'module';
+
 class JavascriptModule{
     constructor(){
         this.accept = 'js'
     }
     async resolve(filepath, { req, res }){
-        require(filepath)({req, res})
+        let contentBuffer = await fs.readFileSync(filepath);
+
+        let result = babel.transform(contentBuffer.toString(), {
+            presets: ["@babel/preset-env"]
+        });
+
+        let moduleInstance = new Module('', module.parent);
+
+        moduleInstance._compile(result.code,'');
+        moduleInstance.exports.default({ req,res });
     }
 }
 
